307398a2c24e37c3090da5acaf14bac3ede86bfb
sruiNascimento <sruy19@gmail.com>
2022-11-10 23:48:31 -0300

    folder out will be ignored

--- src/br/com/alura/chapter2/model/Usuario.java
+++ src/br/com/alura/chapter2/model/Usuario.java
@@ -1,2 +1,28 @@
-package br.com.alura.chapter2.model;public class Usuario {
+package br.com.alura.chapter2.model;
+
+class Usuario {
+    private String nome;
+    private int pontos;
+    private boolean moderador;
+
+    public Usuario(String nome, int pontos) {
+        this.nome = nome;
+        this.pontos = pontos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setModerador() {
+        this.moderador = true;
+    }
+
+    public boolean isModerador() {
+        return moderador;
+    }
 }
